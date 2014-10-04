8035d75ac7292bb7204d538cfb6d588b0ae4bbbc
Efflam Castel <efflamm.castel@gmail.com>
2014-10-04 14:41:51 +0200

    replace temp with chain example

--- lib/refactoring_examples.rb
+++ lib/refactoring_examples.rb
@@ -4,6 +4,7 @@ require 'refactoring_examples/extract_method2'
 require 'refactoring_examples/inline_method1'
 require 'refactoring_examples/inline_temp1'
 require 'refactoring_examples/replace_temp_with_query1'
+require 'refactoring_examples/replace_temp_with_chain1'
 
 module RefactoringExamples
   # Your code goes here...
@@ -13,4 +14,5 @@ include ExtractMethod
 include InlineMethod
 include InlineTemp
 include ReplaceTempWithQuery
+include ReplaceTempWithChain
 
